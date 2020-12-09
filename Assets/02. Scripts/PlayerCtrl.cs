cbe11ac00d39667c74c6bfa95e1d090ac366d17b
on_off <63770983+ckdgus1010@users.noreply.github.com>
2020-12-09 17:58:49 +0900

    201209 p.125까지

---
+++ Assets/02. Scripts/PlayerCtrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private float h = 0.0f;
+    private float v = 0.0f;
+
+    private Transform tr;
+    public float moveSpeed = 10.0f;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Debug.Log("h=" + h.ToString());
+        Debug.Log("v=" + v.ToString());
+
+        //Translate(이동 방향 * 속도 * 변위값 * Time.deltaTime, 기준 좌표)
+        tr.Translate(Vector3.forward * moveSpeed * v * Time.deltaTime, Space.Self);
+    }
+}
